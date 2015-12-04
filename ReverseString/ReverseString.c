f8e34d07810c430e2d0547fef20f17978c8a8195
ZFN <qq674449744@163.com>
2015-12-04 22:39:56 +0800

    Add reverse string function

---
+++ ReverseString/ReverseString.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+char Stack[1024];
+int pEsp;
+
+void Push(char ch)
+{
+	if(pEsp == 1023)
+	{
+		printf("Out of Resource!\n");
+		return ;
+	}
+
+	Stack[pEsp] = ch;
+	pEsp++;
+}
+
+char Pop(void)
+{
+	char ch;
+
+	if(pEsp < 0)
+	{
+		printf("Invalid Data!\n");
+		return 1;
+	}
+
+	pEsp--;
+	ch = Stack[pEsp];
+
+	return ch;
+}
+
+int main(void)
+{
+	char Str[1024];
+	int i,j;
+
+//	scanf("%s", Str);
+	gets(Str);
+
+	pEsp = 0;
+	i = 0;
+	while(Str[i] != '\0')
+	{
+		Push(Str[i]);
+		i++;
+	}
+
+	j = 0;
+	while(i > 0)
+	{
+		Str[j] = Pop();
+		j++;
+		i--;
+	}
+	Str[j] = '\0';
+
+	puts(Str);
+
+	return 0;
+}
